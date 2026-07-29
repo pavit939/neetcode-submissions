27898fea30654277390de0809c5ef8a6e992345f
Pavitra Mohandas <40062888+pavit939@users.noreply.github.com>
2026-07-28 18:23:21 -0700

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.py
@@ -0,0 +1,34 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # 4 operations
+        # You need to iterate from beginning of stack, how? Convert stack to deque + int
+        # when popping from left, check if it's arithmetic or a number.
+        # If arithmetic, maintain a stack which contains only number and until that point pop and have a result.
+        # Store the result back in the stack.
+        # Return result in stack.
+        result = []
+
+        for token in tokens:
+            if token not in {"+", "-", "*", "/"}:
+                result.append(int(token))
+            else:
+                right = result.pop()
+                left = result.pop()
+                if token == "+":
+                    res = left + right   
+                elif token == "-":
+                    res = left - right
+                elif token == "*":
+                    res = left * right
+                elif token == "/":
+                    res = int(left / right)
+                result.append(res)
+        return result[-1]
+
+
+            
+                
+
+        
+
+        
